a93d6a5a472c1df7351e0a2b4cbec2ad4d47bb44
kimyoungjae <sskim5421@gmail.com>
2022-05-04 03:05:34 +0900

    refactor: change naming case

--- src/components/record/index.ts
+++ src/components/record/index.ts
@@ -1,5 +1,5 @@
-export { default as Option } from './option';
-export { default as QuestionWithScore } from './questionWithScore';
-export { default as BottomButton } from './bottomButton';
-export { default as PurposeOfVisit } from './purposeOfVisit';
+export { default as Option } from './Option';
+export { default as QuestionWithScore } from './QuestionWithScore';
+export { default as BottomButton } from './BottomButton';
+export { default as PurposeOfVisit } from './PurposeOfVisit';
 export { default as EatingMenu } from './EatingMenu';
