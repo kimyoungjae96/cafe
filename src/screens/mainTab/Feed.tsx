c4bc3595f11bc3d0a29fa5f934ed5c7ac35492d8
kimyoungjae <sskim5421@gmail.com>
2022-07-16 23:09:55 +0900

    refactor: style code

--- src/screens/mainTab/Feed.tsx
+++ src/screens/mainTab/Feed.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, ScrollView } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import { View, Text, TouchableOpacity } from '@/components';
 import FastImage from 'react-native-fast-image';
 import { heartIcon, logoIcon, messageIcon, starIcon } from '@/assets/images';
@@ -39,23 +39,11 @@ const Post = () => {
         navigation.navigate(ScreenName.CafeDetail);
       }}
       style={{ backgroundColor: 'white', marginBottom: 6 }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          marginTop: 20,
-          paddingHorizontal: 20,
-          justifyContent: 'space-between',
-        }}>
-        <Text style={{ fontSize: 17, fontWeight: '700' }}>카페 알베르</Text>
-        <Text style={{ fontWeight: '500' }}>유저닉네임</Text>
+      <View style={styles.cafeNameAndUserNameWrapper}>
+        <Text style={{ fontSize: 16, fontWeight: '700' }}>카페 알베르</Text>
+        <Text style={{ fontSize: 12, fontWeight: '500' }}>유저닉네임</Text>
       </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          marginTop: 8,
-          alignItems: 'center',
-          paddingHorizontal: 18,
-        }}>
+      <View style={styles.locationAndScoreWrapper}>
         <FastImage
           source={starIcon}
           style={{ width: 16, height: 16 }}
@@ -64,18 +52,8 @@ const Post = () => {
         <Text style={{ fontWeight: '500', fontSize: 13, marginLeft: 2 }}>
           4.5
         </Text>
-        <Text style={{ fontWeight: '500', fontSize: 13, marginLeft: 4 }}>
-          ·
-        </Text>
-        <Text
-          style={{
-            fontSize: 12,
-            fontWeight: '500',
-            color: '#999999',
-            textDecorationLine: 'underline',
-          }}>
-          서울 강남구
-        </Text>
+        <Text style={styles.locationDot}>·</Text>
+        <Text style={styles.locationText}>서울 강남구</Text>
       </View>
       <View
         style={{
@@ -90,25 +68,11 @@ const Post = () => {
       <Text style={{ marginTop: 20, paddingHorizontal: 20 }}>
         커피가 넘 맛있어요~ 분위기도 최고!!
       </Text>
-      <View
-        style={{
-          paddingHorizontal: 20,
-          marginTop: 10,
-          marginBottom: 22,
-          flexDirection: 'row',
-        }}>
+      <View style={styles.tagContainer}>
         <Tag />
         <Tag />
       </View>
-      <View
-        style={{
-          flex: 1,
-          marginHorizontal: 20,
-          backgroundColor: '#efefef',
-          height: 1,
-          marginBottom: 17,
-        }}
-      />
+      <View style={styles.divider} />
       <View
         style={{
           flexDirection: 'row',
@@ -144,28 +108,66 @@ const Post = () => {
 
 const Tag = () => {
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        width: 79,
-        paddingVertical: 10,
-        paddingHorizontal: 16,
-        marginRight: 6,
-        backgroundColor: '#FFF5EF',
-        borderRadius: 50,
-        alignItems: 'center',
-      }}>
+    <View style={styles.tagWrapper}>
       <Text>📖</Text>
-      <Text
-        style={{
-          fontWeight: '500',
-          color: theme['color-primary-500'],
-          marginLeft: 6,
-        }}>
-        태그
-      </Text>
+      <Text style={styles.tagText}>태그</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  cafeNameAndUserNameWrapper: {
+    flexDirection: 'row',
+    marginTop: 20,
+    paddingHorizontal: 20,
+    justifyContent: 'space-between',
+  },
+  tagContainer: {
+    paddingHorizontal: 20,
+    marginTop: 10,
+    marginBottom: 22,
+    flexDirection: 'row',
+  },
+  tagWrapper: {
+    flexDirection: 'row',
+    width: 79,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    marginRight: 6,
+    backgroundColor: '#FFF5EF',
+    borderRadius: 50,
+    alignItems: 'center',
+  },
+  tagText: {
+    fontWeight: '500',
+    color: theme['color-primary-500'],
+    marginLeft: 6,
+  },
+  divider: {
+    flex: 1,
+    marginHorizontal: 20,
+    backgroundColor: '#efefef',
+    height: 1,
+    marginBottom: 17,
+  },
+  locationAndScoreWrapper: {
+    flexDirection: 'row',
+    marginTop: 8,
+    alignItems: 'center',
+    paddingHorizontal: 18,
+  },
+  locationDot: {
+    fontWeight: '500',
+    fontSize: 13,
+    marginLeft: 4,
+    color: '#999999',
+  },
+  locationText: {
+    fontSize: 12,
+    fontWeight: '500',
+    color: '#999999',
+    textDecorationLine: 'underline',
+  },
+});
 
 export default FeedScreen;
